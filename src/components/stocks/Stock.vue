5f0741a6880efd448ae5c9dcc5b67de4abce8dc5
Steven <29716703+DuboisS@users.noreply.github.com>
2020-04-19 17:14:41 +0200

    Stocks page with fake data

--- src/components/stocks/Stock.vue
+++ src/components/stocks/Stock.vue
@@ -1,3 +1,46 @@
 <template>
-	<h1>The Stocks - Stock Component</h1>
+    <div class="col-sm-6 col-md-4">
+        <div class="card">
+            <h5 class="card-header">{{ stock.name}} <small>(${{ stock.price }})</small></h5>
+            <div class="card-body">
+                <div class="float-left">
+                    <input
+                            type="number"
+                            class="form-control"
+                            placeholder="Quantity"
+                            v-model.number="quantity"
+                    >
+                </div>
+                <div class="float-right">
+                    <button
+                            class="btn btn-success"
+                            @click="buyStock"
+                            :disabled="quantity <= 0 || !Number.isInteger(quantity)"
+                    >Buy
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
+
+<script>
+    export default {
+        props: ['stock'],
+        data() {
+            return {
+                quantity: 0
+            }
+        },
+        methods: {
+            buyStock() {
+                const order = {
+                    stockId: this.stock.id,
+                    stockPrice: this.stock.price,
+                    quantity: this.quantity
+                };
+                this.quantity = 0;
+            }
+        }
+    }
+</script>
