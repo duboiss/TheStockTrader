cdadbb560168c55504d0e6b9e4002ac9210f3ceb
Steven <29716703+DuboisS@users.noreply.github.com>
2020-04-22 16:25:08 +0200

    Add end day feature

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -17,7 +17,7 @@
             </ul>
             <ul class="navbar-nav my-2 my-lg-0">
                 <li class="nav-item">
-                    <a class="nav-link" href="#">End Day</a>
+                    <a class="nav-link" href="#" @click="endDay">End Day</a>
                 </li>
                 <li class="nav-item dropdown">
                     <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
@@ -36,10 +36,20 @@
 </template>
 
 <script>
+    import {mapActions} from 'vuex';
+
     export default {
         computed: {
             funds() {
                 return this.$store.getters.funds;
+            }
+        },
+        methods: {
+            ...mapActions([
+               'randomizeStocks'
+            ]),
+            endDay() {
+                this.randomizeStocks();
             }
         }
     }
