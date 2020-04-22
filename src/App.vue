414051ad28775b159f588de3f360b15fd96f7ba2
Steven <29716703+DuboisS@users.noreply.github.com>
2020-04-22 16:34:05 +0200

    Add slide animation to routes transitions

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
     <div class="container">
         <app-header></app-header>
-        <router-view></router-view>
+        <transition name="slide" mode="out-in">
+            <router-view></router-view>
+        </transition>
     </div>
 </template>
 
@@ -32,5 +34,35 @@
 <style>
     body {
         padding: 30px;
+    }
+
+    .slide-enter-active {
+        animation: slide-in 400ms ease-out forwards;
+    }
+
+    .slide-leave-active {
+        animation: slide-out 400ms ease-out forwards;
+    }
+
+    @keyframes slide-in {
+        from {
+            transform: translateY(-30px);
+            opacity: 0;
+        }
+        to {
+            transform: translateY(0);
+            opacity: 1;
+        }
+    }
+
+    @keyframes slide-out {
+        from {
+            transform: translateY(0);
+            opacity: 1;
+        }
+        to {
+            transform: translateY(-30px);
+            opacity: 0;
+        }
     }
 </style>
