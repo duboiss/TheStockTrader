5aa7ea5de96786d7722c96fec273d2f879798b5a
Steven <29716703+DuboisS@users.noreply.github.com>
2020-04-23 18:50:14 +0200

    Handle header dropdown in javascript

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -19,12 +19,16 @@
                 <li class="nav-item">
                     <a class="nav-link" href="#" @click="endDay">End Day</a>
                 </li>
-                <li class="nav-item dropdown">
+                <li class="nav-item dropdown" @click="isDropdownOpen = !isDropdownOpen">
                     <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
                        data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                         Save & Load
                     </a>
-                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                    <div
+                            class="dropdown-menu"
+                            :class="{show: isDropdownOpen}"
+                            aria-labelledby="navbarDropdown"
+                    >
                         <a class="dropdown-item" href="#">Save Data</a>
                         <a class="dropdown-item" href="#">Load Data</a>
                     </div>
@@ -39,6 +43,11 @@
     import {mapActions} from 'vuex';
 
     export default {
+        data() {
+            return {
+                isDropdownOpen: false
+            }
+        },
         computed: {
             funds() {
                 return this.$store.getters.funds;
