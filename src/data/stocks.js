0f311f66dd5710f37dea430eb0f8809838f5c675
Steven DUBOIS <git@duboiss.fr>
2021-02-10 23:52:48 +0100

    Add more stocks

--- src/data/stocks.js
+++ src/data/stocks.js
@@ -2,5 +2,8 @@ export default [
     {id: 1, name: 'BMW', price: 110},
     {id: 2, name: 'Google', price: 200},
     {id: 3, name: 'Apple', price: 250},
-    {id: 4, name: 'Twitter', price: 8},
+    {id: 4, name: 'Twitter', price: 60},
+	{id: 5, name: 'Tesla', price: 150},
+	{id: 6, name: 'Alstom', price: 80},
+	{id: 7, name: 'Total', price: 90}
 ];
